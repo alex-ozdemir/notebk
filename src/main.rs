aae7d777ab4642feb2f0078561a5ba0644d13bea
Alex Ozdemir <aozdemir@hmc.edu>
2018-05-06 11:24:21 -0400

    Configurable directory

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,51 @@ extern crate chrono;
 
 use std::env;
 use std::fs;
+use std::path::{Path, PathBuf};
 use std::io::BufRead;
+use std::io::Read;
 use std::io;
 
-const DIRECTORY: &str = "/Users/aozdemir/personal/notebook";
+fn cfgs() -> Option<(PathBuf, PathBuf)> {
+    env::home_dir().map(|mut p| {
+        let mut p2 = p.clone();
+        p.push(".notebk");
+        p2.push(".config");
+        p2.push("notebk");
+        (p, p2)
+    })
+}
 
-fn entries() -> Result<Vec<fs::DirEntry>, std::io::Error> {
+fn get_directory() -> io::Result<String> {
+    cfgs()
+        .map(|(p, p2)| read_file(p).or_else(|_| read_file(p2)))
+        .unwrap_or_else(|| {
+            Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "Can't identify your $HOME",
+            ))
+        })
+}
+
+fn read_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
+    fs::File::open(path.as_ref()).and_then(|mut f| {
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).map(|_| {
+            let l = contents.as_str().trim_right().len();
+            contents.truncate(l);
+            contents
+        })
+    })
+}
+
+fn entries() -> io::Result<Vec<fs::DirEntry>> {
     let mut listing: Vec<_> = Vec::new();
-    for entry_result in fs::read_dir(DIRECTORY)? {
+    for entry_result in fs::read_dir(get_directory()?)? {
         listing.push(entry_result?);
     }
-    listing.as_mut_slice().sort_unstable_by_key(
-        |e| e.file_name(),
-    );
+    listing
+        .as_mut_slice()
+        .sort_unstable_by_key(|e| e.file_name());
     listing.as_mut_slice().reverse();
     Ok(listing)
 }
@@ -49,7 +81,7 @@ fn open_entry(n: usize) -> Result<(), std::io::Error> {
 }
 
 fn open_todays_entry() -> io::Result<()> {
-    let mut path_buf = std::path::PathBuf::from(DIRECTORY);
+    let mut path_buf = std::path::PathBuf::from(get_directory()?);
     path_buf.push(format!("{}", chrono::Local::now().format("%Y-%m-%d.md")));
     std::process::Command::new("vim").arg(path_buf).status()?;
     Ok(())
@@ -57,6 +89,11 @@ fn open_todays_entry() -> io::Result<()> {
 
 fn delete_entry(n: usize) -> io::Result<()> {
     fs::remove_file(entry(n)?.path())
+}
+
+fn identify_entry(n: usize) -> io::Result<()> {
+    println!("{}", entry(n)?.path().to_string_lossy());
+    Ok(())
 }
 
 fn number_arg(n: usize) -> io::Result<usize> {
@@ -73,17 +110,17 @@ fn application() -> io::Result<()> {
     match env::args().nth(1).as_ref().map(String::as_str) {
         None => open_todays_entry(),
         Some("delete") => delete_entry(number_arg(2)?),
+        Some("which") => identify_entry(number_arg(2)?),
         Some("ls") => list(number_arg(2).unwrap_or(10)),
         Some(_) => open_entry(number_arg(1)?),
     }
 }
 
 fn main() {
-    match application() {
-        Err(e) => {
-            println!("Error: {}", e);
-            std::process::exit(1);
-        }
-        Ok(()) => std::process::exit(0),
-    }
+    std::process::exit(
+        application()
+            .map_err(|e| println!("Error: {}", e))
+            .map(|_| 0)
+            .unwrap_or(1),
+    )
 }
